25babb54990239e3b7ab339da1712bc78a559a55
AppeazeTheCheese <yolosolodbaz@gmail.com>
2023-11-20 06:10:14 +0000

    Built script

--- dist/fire_extra.user.js
+++ dist/fire_extra.user.js
@@ -4,24 +4,9 @@
 // @author       double-beep
 // @contributor  Xnero
 // @description  Watch, blacklist and see domain stats directly from the FIRE popup!
-// @match       *://chat.stackexchange.com/transcript/*
-// @match       *://chat.meta.stackexchange.com/transcript/*
-// @match       *://chat.stackoverflow.com/transcript/*
-// @match       *://chat.stackexchange.com/users/120914/*
-// @match       *://chat.stackexchange.com/users/120914?*
-// @match       *://chat.stackoverflow.com/users/3735529/*
-// @match       *://chat.stackoverflow.com/users/3735529?*
-// @match       *://chat.meta.stackexchange.com/users/266345/*
-// @match       *://chat.meta.stackexchange.com/users/266345?*
-// @match       *://chat.stackexchange.com/users/478536/*
-// @match       *://chat.stackexchange.com/users/478536?*
-// @match       *://chat.stackoverflow.com/users/14262788/*
-// @match       *://chat.stackoverflow.com/users/14262788?*
-// @match       *://chat.meta.stackexchange.com/users/848503/*
-// @match       *://chat.meta.stackexchange.com/users/848503?*
-// @include     /^https?://chat.stackexchange.com/(?:rooms/|search.*[?&]room=)(?:11|27|95|201|388|468|511|2165|3877|8089|11540|22462|24938|34620|35068|38932|46061|47869|56223|58631|59281|61165|65945|84778|96491|106445|109836|109841|129590)(?:[&/].*$|$)/
-// @include     /^https?://chat.meta.stackexchange.com/(?:rooms/|search.*[?&]room=)(?:89|1037|1181)(?:[&/].*$|$)/
-// @include     /^https?://chat.stackoverflow.com/(?:rooms/|search.*[?&]room=)(?:41570|90230|111347|126195|167826|170175|202954)(?:[&/].*$|$)/
+// @match        *://chat.stackexchange.com/rooms/11540/charcoal-hq?*
+// @match        *://chat.stackexchange.com/transcript/11540*
+// @match        *://chat.stackexchange.com/transcript/message/*
 // @grant        GM_xmlhttpRequest
 // @grant        GM_addStyle
 // @run-at       document-start
