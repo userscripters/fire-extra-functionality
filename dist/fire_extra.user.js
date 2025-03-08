79702812a367198e0a1b0719c0fcc162498027b8
double beep <38133098+double-beep@users.noreply.github.com>
2025-03-08 10:58:33 +0000

    1.4.3

--- dist/fire_extra.user.js
+++ dist/fire_extra.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         FIRE Additional Functionality
-// @version      1.4.2
+// @version      1.4.3
 // @author       double-beep
 // @contributor  Xnero
 // @description  Watch, blacklist and see domain stats directly from the FIRE popup!
